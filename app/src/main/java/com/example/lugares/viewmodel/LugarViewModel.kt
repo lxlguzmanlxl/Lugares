7f06d63be5a4f4cf6f5db5b36e78deba46336148
lxlguzmanlxl <lguzman30716@ufide.ac.cr>
2022-10-18 21:26:31 -0600

    Estructura Commit

---
+++ app/src/main/java/com/example/lugares/viewmodel/LugarViewModel.kt
@@ -0,0 +1,38 @@
+package com.example.lugares.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.*
+import com.example.lugares.data.LugarDatabase
+import com.example.lugares.model.Lugar
+import com.example.lugares.repository.LugarRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class LugarViewModel (application: Application) : AndroidViewModel(application) {
+    //Lista que contiene objetos de tipo lugar
+    val getAllData : LiveData<List<Lugar>>
+
+    private val repository : LugarRepository
+
+    init {
+        val lugarDao = LugarDatabase.getDatabase(application).lugarDao()
+        //Inicializa repository
+        repository = LugarRepository(lugarDao)
+        getAllData = repository.getAllData
+    }
+
+    fun addLugar (lugar: Lugar){
+        //Se crean las tareas de entrada y salida para cada function
+        viewModelScope.launch(Dispatchers.IO){ repository.addLugar(lugar) }
+    }
+
+    fun updateLugar (lugar: Lugar){
+        //Se crean las tareas de entrada y salida para cada function
+        viewModelScope.launch(Dispatchers.IO){ repository.updateLugar(lugar) }
+    }
+
+    fun deleteLugar (lugar: Lugar){
+        //Se crean las tareas de entrada y salida para cada function
+        viewModelScope.launch(Dispatchers.IO){ repository.deleteLugar(lugar) }
+    }
+}
